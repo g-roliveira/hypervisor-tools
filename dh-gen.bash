dabac090f098ab41f921b526b4a61642b9e0980b
HPPinata <83947761+HPPinata@users.noreply.github.com>
2021-09-29 22:36:51 +0200

    Update dh-gen.bash

--- dh-gen.bash
+++ dh-gen.bash
@@ -6,10 +6,15 @@ LIMIT=8192
 
 while (( $SIZE <= $LIMIT ))
 do
-	ssh-keygen -M generate -O bits=$SIZE candidates$SIZE
-	
-	
-	
+	ssh-keygen -M generate -O bits=$SIZE candidates$SIZE &
+	(( SIZE = $SIZE + $ADD ))
+done
+wait
+SIZE=2048
+while (( $SIZE <= $LIMIT ))
+do
+	ssh-keygen -M screen -f candidates$SIZE
+		rm candidates$SIZE
 	(( SIZE = $SIZE + $ADD ))
 done
 mv /etc/ssh/moduli /etc/ssh/mod.back

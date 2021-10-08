a87e380a1401b36d416e3f2cdd43d6b39266145c
HPPinata <83947761+HPPinata@users.noreply.github.com>
2021-10-08 15:17:00 +0200

    Update dh-gen.bash

--- dh-gen.bash
+++ dh-gen.bash
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+#set minimum and maximum key size, as well as step size
 SIZE=2048
 ADD=1024
 LIMIT=8192
 
+#create and go to empty temporary directory
 rm -r temp
 mkdir temp && cd temp
 
+#start first stage (generation), runs multiple sizes in parallel
 while (( $SIZE <= $LIMIT ))
 do
 	ssh-keygen -M generate -O bits=$SIZE candidates$SIZE &
@@ -14,9 +17,11 @@ do
 done
 wait
 
+#reset size to start value for second stage
 sleep 1
 SIZE=2048
 
+#start second stage (validation), runs multiple sizes in parallel
 while (( $SIZE <= $LIMIT ))
 do
 	ssh-keygen -M screen -f candidates$SIZE "$SIZE".f &
@@ -24,9 +29,11 @@ do
 done
 wait
 
+#remove files from first stage and create sorted, final file
 rm candidates*
 cat $(ls -v) > final
 
+#move file to correct location and delete temporary directory
 mv /etc/ssh/moduli /etc/ssh/mod.back
 mv final /etc/ssh/moduli
 cd .. && rm -r temp

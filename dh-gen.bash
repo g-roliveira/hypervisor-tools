1e284fa5a01a848ab9fe8fd33a45b44a0493da5c
HPPinata <83947761+HPPinata@users.noreply.github.com>
2021-11-06 13:16:19 +0100

    Update dh-gen.bash

--- dh-gen.bash
+++ dh-gen.bash
@@ -11,7 +11,7 @@ mkdir temp && cd temp
 #start first stage (generation), runs multiple sizes in parallel
 while (( $SIZE <= $LIMIT ))
 do
-	ssh-keygen -M generate -O bits=$SIZE candidates$SIZE &
+	ssh-keygen -M generate -O bits=$SIZE "$SIZE".c &
 	(( SIZE = $SIZE + $STEP ))
 done
 wait
@@ -23,16 +23,17 @@ SIZE=2048
 #start second stage (validation), runs multiple sizes in parallel
 while (( $SIZE <= $LIMIT ))
 do
-	ssh-keygen -M screen -f candidates$SIZE "$SIZE".f &
+	ssh-keygen -M screen -f "$SIZE".c "$SIZE".f &
 	(( SIZE = $SIZE + $STEP ))
 done
 wait
 
 #remove files from first stage and create sorted, final output
-rm candidates*
-cat $(ls -v) > final
+rm *.c
+cat $(ls -v) > moduli
 
 #move file to correct location and delete temporary directory
+cp moduli ../moduli
 mv /etc/ssh/moduli /etc/ssh/mod.back
-mv final /etc/ssh/moduli
+mv moduli /etc/ssh/moduli
 cd .. && rm -r temp

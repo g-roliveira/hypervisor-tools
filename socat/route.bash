dde7341098cb290d05bb7b569d1a996f85521f03
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-02-22 12:15:36 +0100

    add screen

--- socat/route.bash
+++ socat/route.bash
@@ -1,4 +1,4 @@
-socat TCP6-LISTEN:80,reuseaddr,fork TCP4:localhost:80
-socat TCP6-LISTEN:443,reuseaddr,fork TCP4:localhost:443
-socat TCP6-LISTEN:3478,reuseaddr,fork TCP4:localhost:3478
-socat UDP6-LISTEN:3478,reuseaddr,fork UDP4:localhost:3478
+screen -dmS so80 socat TCP6-LISTEN:80,reuseaddr,fork TCP4:localhost:80
+screen -dmS so443 socat TCP6-LISTEN:443,reuseaddr,fork TCP4:localhost:443
+screen -dmS so3478 socat TCP6-LISTEN:3478,reuseaddr,fork TCP4:localhost:3478
+screen -dmS so3478 socat UDP6-LISTEN:3478,reuseaddr,fork UDP4:localhost:3478

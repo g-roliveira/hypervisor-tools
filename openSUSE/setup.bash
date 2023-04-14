6b4a5170cbddf47ea6fbe42b5e5f02cc337b06d4
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-04-14 11:31:59 +0200

    Create setup.bash

---
+++ openSUSE/setup.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo 'default' > /etc/hostname
+echo 'PermitRootLogin yes' > /etc/ssh/sshd_config.d/root.conf
+
+mount /dev/sr0 /mnt
+
+zypper rm -yu xen-tools-domU
+/mnt/Linux/install.sh -d sles -m 15 -n
+
+zypper in -y nano zram-generator
+systemctl enable docker
+
+cat <<'EOL' > /etc/systemd/zram-generator.conf
+[zram0]
+
+zram-size = ram
+compression-algorithm = zstd
+EOL
+
+sed -i "s+SELINUX=enforcing+SELINUX=permissive+g" /etc/selinux/config

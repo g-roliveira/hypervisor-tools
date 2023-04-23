3f148f2c413802bc6f98650a7f0eeb301ed3f02c
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-04-23 15:12:08 +0200

    no inplace update

--- proxmox/dhcp.bash
+++ proxmox/dhcp.bash
@@ -1,1 +1,2 @@
-sed -i '1h;1!H;${g;s/iface vmbr0.*10.1/iface vmbr0 inet dhcp/;p;}' /etc/network/interfaces
+cp /etc/network/interfaces /etc/network/interfaces.bak
+sed -n '1h;1!H;${g;s/iface vmbr0.*10.1/iface vmbr0 inet dhcp/;p;}' /etc/network/interfaces.bak > /etc/network/interfaces

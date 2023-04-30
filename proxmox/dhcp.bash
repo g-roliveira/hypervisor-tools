ff908e87fffff521d50d2c7bab5ae19d776a824c
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-04-30 21:59:20 +0200

    show config

--- proxmox/dhcp.bash
+++ proxmox/dhcp.bash
@@ -1,2 +1,3 @@
 cp /etc/network/interfaces /etc/network/interfaces.bak
 sed -n '1h;1!H;${g;s/iface vmbr0.*10.1/iface vmbr0 inet dhcp/;p;}' /etc/network/interfaces.bak > /etc/network/interfaces
+cat /etc/network/interfaces

300c2d92ef594620c8890c0200c9fbd3c4426a6c
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-05-11 10:08:13 +0200

    change listname

--- proxmox/repo.bash
+++ proxmox/repo.bash
@@ -1,6 +1,6 @@
 mv /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
 
-cat <<'EOL' > /etc/apt/sources.list.d/community.list
+cat <<'EOL' > /etc/apt/sources.list.d/pve-no-subscription.list
 # PVE pve-no-subscription repository provided by proxmox.com,
 # NOT recommended for production use
 deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription

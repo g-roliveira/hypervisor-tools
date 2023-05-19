b1ae2766790c5a6413a843dc3b98831a2b975e90
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-05-19 17:10:59 +0200

    grub console

--- proxmox/pcie.bash
+++ proxmox/pcie.bash
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cat <<'EOL' > /etc/modules-load.d/vfio.conf
 vfio
 vfio_iommu_type1
@@ -10,17 +12,12 @@ PCI=$(lspci -n -s 01:00 | awk -F ': ' {'print $2'} | awk -F ' ' {'print $1'})
 echo "options vfio-pci ids=$(echo $PCI | sed 's/ /,/g') disable_vga=1" > /etc/modprobe.d/vfio.conf
 cat /etc/modprobe.d/vfio.conf
 
-echo "blacklist amdgpu" >> /etc/modprobe.d/blacklist.conf
-echo "blacklist radeon" >> /etc/modprobe.d/blacklist.conf
-echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf
-echo "blacklist nvidia" >> /etc/modprobe.d/blacklist.conf
-cat /etc/modprobe.d/blacklist.conf
-
-sed -i -E 's/(GRUB_CMDLINE_LINUX_DEFAULT=".*)"/\1 amd_iommu=on iommu=pt video=vesafb:off,efifb:off pcie_acs_override=downstream,multifunction"/g' /etc/default/grub
-cat /etc/default/grub
-
 echo "options kvm ignore_msrs=1 report_ignored_msrs=0" > /etc/modprobe.d/kvm.conf
 cat /etc/modprobe.d/kvm.conf
+
+sed -i -E 's/(GRUB_CMDLINE_LINUX_DEFAULT=".*)"/\1 amd_iommu=on iommu=pt pcie_acs_override=downstream,multifunction"/g' /etc/default/grub
+sed -i -E 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/g' /etc/default/grub
+cat /etc/default/grub
 
 update-initramfs -u -k all
 update-grub

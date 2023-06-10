2bc635017d3abe85618654f631cbfa042759eac4
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-06-10 16:26:57 +0200

    Delete dhcp.bash

--- proxmox/scripts/dhcp.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp /etc/network/interfaces /etc/network/interfaces.bak
-sed -n '1h;1!H;${g;s/iface vmbr0.*10.1/iface vmbr0 inet dhcp/;p;}' /etc/network/interfaces.bak > /etc/network/interfaces
-cat /etc/network/interfaces

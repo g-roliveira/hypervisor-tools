138c9c5ea7d03873e557ad54df5ae8d4cbd80f59
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-06-11 17:23:45 +0200

    add vmbr0 if not present

--- proxmox/scripts/dhcp.bash
+++ proxmox/scripts/dhcp.bash
@@ -2,4 +2,20 @@
 
 cp /etc/network/interfaces /etc/network/interfaces.bak
 sed -n '1h;1!H;${g;s/iface vmbr0.*10.1/iface vmbr0 inet dhcp/;p;}' /etc/network/interfaces.bak > /etc/network/interfaces
+
+VAR=$(cat <<'EOL'
+iface enp8s0 inet manual
+
+auto vmbr0
+iface vmbr0 inet dhcp
+        bridge-ports enp8s0
+        bridge-stp off
+        bridge-fd 0
+EOL
+)
+VAR=$(VAR=${VAR@Q}; echo "${VAR:2:-1}")
+
+sed -i "s+auto enp8s0++g" /etc/network/interfaces
+sed -i "s+iface enp8s0 inet dhcp+$VAR+g" /etc/network/interfaces
+
 cat /etc/network/interfaces

ce25d64bc226b5ed96719c4cef0918370902892c
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-06-16 14:03:36 +0200

    formatting

--- proxmox/scripts/dhcp.bash
+++ proxmox/scripts/dhcp.bash
@@ -3,20 +3,22 @@
 cp /etc/network/interfaces /etc/network/interfaces.bak
 sed -n '1h;1!H;${g;s/iface vmbr0.*10.1/iface vmbr0 inet dhcp/;p;}' /etc/network/interfaces.bak > /etc/network/interfaces
 
-VAR=$(cat <<'EOL'
-iface enp8s0 inet manual
+sed -i "s+inet auto+inet dhcp+g" /etc/network/interfaces
+sed -i "s+inet6 auto+inet6 dhcp+g" /etc/network/interfaces
 
+VAR=$(cat <<'EOL'
+
+iface enp8s0 inet manual
 auto vmbr0
 iface vmbr0 inet dhcp
         bridge-ports enp8s0
         bridge-stp off
         bridge-fd 0
+
 EOL
 )
-VAR=$(VAR=${VAR@Q}; echo "${VAR:2:-1}")
 
+VAR=$(VAR=${VAR@Q}; echo "${VAR:2:-1}")
 sed -i "s+iface enp8s0 inet dhcp+$VAR+g" /etc/network/interfaces
-sed -i "s+inet auto+inet dhcp+g" /etc/network/interfaces
-sed -i "s+inet6 auto+inet6 dhcp+g" /etc/network/interfaces
 
 cat /etc/network/interfaces

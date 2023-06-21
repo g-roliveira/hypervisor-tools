b110f0855068eff57eba64b7eb3bd8ec579840e4
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-06-21 16:08:26 +0200

    Create back.bash

---
+++ proxmox/scripts/back.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat <<'EOL' > /etc/apt/sources.list.d/pbs-no-subscription.list
+# Proxmox Backup Server pbs-no-subscription repository provided by proxmox.com,
+# NOT recommended for production use
+deb http://download.proxmox.com/debian/pbs bullseye pbs-no-subscription
+EOL
+
+apt update && apt full-upgrade -y && apt autopurge -y
+apt install -y proxmox-backup-server
+
+mv /etc/apt/sources.list.d/pbs-enterprise.list /etc/apt/sources.list.d/pbs-enterprise.list.bak

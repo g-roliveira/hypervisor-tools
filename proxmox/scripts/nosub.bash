74fb65bcdc25e740eb5ed1ef782fcaf4f0e780fe
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-06-21 16:12:22 +0200

    modify PBS

--- proxmox/scripts/nosub.bash
+++ proxmox/scripts/nosub.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 sed -i.bak 's/notfound/active/g' /usr/share/perl5/PVE/API2/Subscription.pm
-systemctl restart pveproxy.service
+systemctl restart pveproxy
+
+sed -i.bak -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+systemctl restart proxmox-backup-proxy

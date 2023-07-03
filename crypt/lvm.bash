6e0396ed658df81977cb30b647e8a11235c96c38
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-07-03 19:32:23 +0200

    nvme drive

--- crypt/lvm.bash
+++ crypt/lvm.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 lvresize -r -L -1G system/root
-pvresize --setphysicalvolumesize XXXX /dev/sda2 #16+ MB smaller than device for LUKS2 header. Shrinking 100+ MB and growing after recommended
+pvresize --setphysicalvolumesize XXXX /dev/nvme0n1p5 #16+ MB smaller than device for LUKS2 header. Shrinking 100+ MB and growing after recommended
 vgchange -an
 
-cryptsetup reencrypt /dev/sda2 --encrypt --reduce-device-size 16M --pbkdf pbkdf2 #GRUB 2.06 only supports pbkdf2
-cryptsetup open /dev/sda2 crypt_root
+cryptsetup reencrypt /dev/nvme0n1p5 --encrypt --reduce-device-size 16M --pbkdf pbkdf2 #GRUB 2.06 only supports pbkdf2
+cryptsetup open /dev/nvme0n1p5 crypt_root
 vgchange -ay
 
 mount /dev/system/root /mnt
@@ -13,7 +13,7 @@ for i in proc run sys dev; do mount --rbind /$i /mnt/$i; done
 chroot /mnt
 
 mount -a
-echo 'crypt_root  /dev/sda2  none' >> /etc/crypttab
+echo 'crypt_root  /dev/nvme0n1p5  none' >> /etc/crypttab
 sed -i 's+GRUB_ENABLE_CRYPTODISK="n"+GRUB_ENABLE_CRYPTODISK="y"+g' /etc/default/grub
 
 mkinitrd

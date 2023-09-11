f0ae81c0f0ff0bba975dd36980aa2ea10ebbcbcb
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-09-11 17:51:14 +0200

    sed regex

--- proxmox/scripts/dhcp.bash
+++ proxmox/scripts/dhcp.bash
@@ -3,9 +3,6 @@
 cp /etc/network/interfaces /etc/network/interfaces.bak
 sed -n '1h;1!H;${g;s/iface vmbr0.*10.1/iface vmbr0 inet dhcp/;p;}' /etc/network/interfaces.bak > /etc/network/interfaces
 
-sed -i "s+inet auto+inet dhcp+g" /etc/network/interfaces
-sed -i "s+inet6 auto+inet6 dhcp+g" /etc/network/interfaces
-
 VAR=$(cat <<'EOL'
 
 iface enp8s0 inet manual
@@ -19,6 +16,6 @@ EOL
 )
 
 VAR=$(VAR=${VAR@Q}; echo "${VAR:2:-1}")
-sed -i "s+iface enp8s0 inet dhcp+$VAR+g" /etc/network/interfaces
+sed -i -E "s+iface enp8s0 inet (auto|dhcp)+$VAR+g" /etc/network/interfaces
 
 cat /etc/network/interfaces

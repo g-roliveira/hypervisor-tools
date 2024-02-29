4b69374f2e2283a08b1a061e162b823cc46b35e0
HPPinata <83947761+HPPinata@users.noreply.github.com>
2024-02-29 21:33:43 +0100

    switch to default branch

--- git/alldirs.ps1
+++ git/alldirs.ps1
@@ -1,8 +1,10 @@
-Get-ChildItem -Recurse -Depth 2 -Force | 
+Get-ChildItem -Recurse -Depth 2 -Force |
 Where-Object { $_.Mode -match "h" -and $_.FullName -like "*\.git" } |
 ForEach-Object {
   cd $_.FullName
   cd ../
   git pull
+  git remote set-head origin -a
+  git switch $(git rev-parse --abbrev-ref HEAD)
   cd ../
 }

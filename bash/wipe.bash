90ad28aed1848b456febda953db5d9d6322a631f
HPPinata <83947761+HPPinata@users.noreply.github.com>
2024-03-10 13:30:57 +0100

    wipe all disks

--- bash/wipe.bash
+++ bash/wipe.bash
@@ -1,3 +1,3 @@
 drive=$(ls /dev/sd*)
-cache=( /dev/nvme0n1 /dev/nvme1n1 )
-dd if=/dev/zero bs=1M count=100 | tee ${cache[@]} ${drive[@]} /dev/nvme2n1 > /dev/zero
+cache=$(ls /dev/nvme*)
+dd if=/dev/zero bs=1M count=100 | tee ${cache[@]} ${drive[@]} > /dev/zero
